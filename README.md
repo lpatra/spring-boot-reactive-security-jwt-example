28c0f3dd04290c6b3e3c61fb200d7ea81c913017
Lalatendu Patra <lpatra@outlook.com>
2021-01-31 17:42:22 +0000

    basic auth implementation with a hardcoded user

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Introduction
 
-This is an example project to show JWT based authentication in a reactive spring boot app. It is still under development, and the code is not working yet.
+This is an example project to show JWT based authentication in a reactive spring boot app. 
+- It is still under development, and the code is not working yet.
+- Only http basic authentication implemented with a hardcoded user
 
 # How it was done?
 - TODO
--- src/main/java/com/example/springbootreactivesecurityjwtexample/security/SecurityConfig.java
+++ src/main/java/com/example/springbootreactivesecurityjwtexample/security/SecurityConfig.java
@@ -1,4 +1,58 @@
 package com.example.springbootreactivesecurityjwtexample.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Value("${spring.security.basic.auth.username}")
+    private String username;
+    @Value("${spring.security.basic.auth.password}")
+    private String password;
+    @Value("${spring.security.basic.auth.roles}")
+    private String roles;
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername(username)
+                .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(password))
+                .roles(roles)
+                .build();
+        return new MapReactiveUserDetailsService(userDetails);
+    }
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(
+                        authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated()
+                )
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (response, error) ->
+                                Mono.fromRunnable(() -> response.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED))
+                )
+                .accessDeniedHandler(
+                        (response, error) ->
+                                Mono.fromRunnable(() -> response.getResponse().setStatusCode(HttpStatus.FORBIDDEN))
+                )
+                .and()
+                .httpBasic(Customizer.withDefaults())
+                .formLogin().disable()
+                .csrf().disable()
+                .build();
+    }
 }
